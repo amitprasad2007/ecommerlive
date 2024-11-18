ad3e35a91413e6b753196300c2949774a301438d
amit <amit.prasad@learningsprial.co.in>
2024-11-19 01:10:18 +0530

    changes

--- app/Http/Kernel.php
+++ app/Http/Kernel.php
@@ -43,6 +43,9 @@ class Kernel extends HttpKernel
             'throttle:100,1',
             \Illuminate\Routing\Middleware\SubstituteBindings::class,
             \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Laravel\Sanctum\Http\Middleware\EnsureFrontendRequestsAreStateful::class,
+            \Illuminate\Routing\Middleware\ThrottleRequests::class.':api',
+            \Illuminate\Routing\Middleware\SubstituteBindings::class,
         ],
     ];
 
