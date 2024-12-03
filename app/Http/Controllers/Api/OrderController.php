c587d08ea789040c8f8c594d72acdb22159b5fc1
amitprasad2007 <amit.prasad2007@rediffmail.com>
2024-12-03 21:33:04 +0530

    changes

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -12,6 +12,6 @@ class OrderController extends Controller
     public function savecart(Request $request){
         //dd($request)
 
-        return response()->json(['user' => Auth::user()]);
+        return response()->json(['req' => $request]);
     }
 }
