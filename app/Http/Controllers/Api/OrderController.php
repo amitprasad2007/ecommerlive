f52d1cc7e9471213476580b0e620ac278fe0b368
amitprasad2007 <amit.prasad2007@rediffmail.com>
2024-12-03 20:04:24 +0530

    changes

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -4,10 +4,13 @@ namespace App\Http\Controllers\Api;
 
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
+use App\User;
 
 class OrderController extends Controller
 {
     public function savecart(Request $request){
+        //dd($request)
+
         return response()->json(['user' => Auth::user()]);
     }
 }
