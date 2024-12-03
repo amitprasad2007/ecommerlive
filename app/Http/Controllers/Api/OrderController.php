d8065d4b1614375f978ee9493a33a2efbbd7b223
amitprasad2007 <amit.prasad2007@rediffmail.com>
2024-12-03 22:21:12 +0530

    changes

--- app/Http/Controllers/Api/OrderController.php
+++ app/Http/Controllers/Api/OrderController.php
@@ -5,6 +5,8 @@ namespace App\Http\Controllers\Api;
 use App\Http\Controllers\Controller;
 use Illuminate\Http\Request;
 use App\User;
+use App\Models\Cart;
+use App\Models\Product;
 use Illuminate\Support\Facades\Auth;
 
 class OrderController extends Controller
@@ -13,11 +15,23 @@ class OrderController extends Controller
         //dd($request)
         $totalcart = $request->cart;
         foreach($totalcart as $cart ){
+            $slug = $cart['slug'];
+            $quantity = $cart['quantity'];
+            $product = Product::where('slug', $slug)->first();
 
-            return response()->json(['req' => $cart['slug']]);      
-
+            $cart = new Cart;
+            $cart->user_id = auth()->user()->id;
+            $cart->product_id = $product->id;
+            $cart->price = ($product->price-($product->price*$product->discount)/100);
+            $cart->quantity = $quantity;
+            $cart->amount=$cart->price*$cart->quantity;
+            $cart->status = 'new';
+            $cart->save();
+            $products[]=$product;
         }
-
+        return response()->json([
+            'product' => $products
+        ]);
       
     }
 }
