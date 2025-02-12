ff669d38b7590a729f65835ffe17c42b0fbb172c
amit <amit.prasad@learningsprial.co.in>
2025-02-12 23:42:52 +0530

    changes

---
+++ app/Http/Controllers/Api/PaymentController.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace App\Http\Controllers\Api;
+
+use App\Http\Controllers\Controller;
+use Illuminate\Http\Request;
+use Razorpay\Api\Api;
+
+
+class PaymentController extends Controller
+{
+    public function createOrder(Request $request)
+       {
+           $api = new Api(env('RAZOR_KEY_ID'), env('RAZOR_KEY_SECRET'));
+
+           $orderData = [
+               'receipt'         => 'rcptid_11',
+               'amount'          => 50000, // Amount in paise
+               'currency'        => 'INR',
+               'payment_capture' => 1 // Auto capture
+           ];
+
+           try {
+               $order = $api->order->create($orderData);
+               return response()->json($order);
+           } catch (\Exception $e) {
+               return response()->json(['error' => $e->getMessage()], 500);
+           }
+       }
+}
