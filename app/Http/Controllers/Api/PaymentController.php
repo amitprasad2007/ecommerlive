14795b44e6933e8a80b73aa01e3c125672b1f499
amit <amit.prasad@learningsprial.co.in>
2025-02-13 00:12:43 +0530

    changes

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -11,10 +11,10 @@ class PaymentController extends Controller
 {
     public function createOrder(Request $request)
        {
-        dd('dfwerwerwerwe');
+       // dd('dfwerwerwerwe');
 
            $api = new Api(env('RAZOR_KEY_ID'), env('RAZOR_KEY_SECRET'));
-
+            dd($api);
            $orderData = [
                'receipt'         => 'rcptid_11',
                'amount'          => 50000, // Amount in paise
