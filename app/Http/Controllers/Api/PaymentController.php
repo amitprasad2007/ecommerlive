b10d051e8f808827a081e1c04fdc0788c60f2ceb
amitprasad2007 <amit.prasad2007@rediffmail.com>
2025-06-18 14:26:54 +0530

    changes

--- app/Http/Controllers/Api/PaymentController.php
+++ app/Http/Controllers/Api/PaymentController.php
@@ -15,72 +15,50 @@ class PaymentController extends Controller
 {
     public function createOrder(Request $request){
         
-        $firstName = $request->shipping['firstName'];
-        $lastName = $request->shipping['lastName']; 
-        $customeremail = $request->shipping['email'];
-        $mobile = $request->shipping['mobile'];
-        $billingAddress = $request->shipping['address'].$request->shipping['address2'];
-        $billingstate = $request->shipping['state'];
-        $billingzip = $request->shipping['postal_code'];
-        $TOTALAMT = $request->total*100;       
-        $productes = $request->items;
-        $orderIds = [];
-        $orderIdstirng = ''; // Initialize the orderIdstirng variable
-        foreach( $productes as $products){
-            $slug = $products['slug'];
-            $quantity = $products['quantity'];
-            $price = $products['price'];
-            $productonly = Product::with('photoproduct')->where('slug', $slug)->first();
-            if (!$productonly) {
-                // Handle the case where the product is not found
-                return response()->json(['error' => 'Product not found'], 404);
-            }
-            $Order = new Order;
-            $Order->user_id = auth()->user()->id;
-            $Order->order_number = 'ORD-' . time() . '-' . bin2hex(random_bytes(5));
-            $Order->sub_total = $productonly->price;
-            $Order->quantity = $products['quantity'];
-            $Order->total_amount = $productonly->price * $products['quantity'];
-            $Order->status = 'new';
-            $Order->payment_method = $request->paymentMethod;
-            $Order->payment_status = "unpaid";
-            $Order->first_name = $firstName;
-            $Order->last_name = $lastName;
-            $Order->email = $customeremail;
-            $Order->product_id =  $productonly->id;
-            $Order->phone =$mobile;
-            $Order->country ='India';
-            $Order->post_code = $billingzip;
-            $Order->address1 = $billingAddress;
-            $Order->address2 = $billingstate;
-            $Order->save();
-            // Check if the order was saved successfully
-            if (!$Order->wasRecentlyCreated) {
-                return response()->json(['error' => 'Order creation failed'], 500);
-            }
-            $cart = Cart::find($products['cart_id']);
+        $order = Order::create([
+            'order_number' => 'ORD-' . time() . '-' . bin2hex(random_bytes(5)), // Generate a unique order ID
+            'user_id' => auth()->user()->id,
+            'address_id' => $request->shipping['id'],
+            'sub_total' => $request->subtotal,
+            'quantity' => $request->totalquantity,
+            'shippingcost' => $request->shippingcost,
+            'tax' => $request->tax,
+            'total_amount' => $request->total,
+            'payment_method' => $request->paymentMethod,
+            'payment_status' => 'unpaid',
+            'status' => 'new',
+            'transaction_id' => $request->payment_method === 'online' ? $request->razorpay_payment_id : null,
+            'payment_details' => json_encode($request->all()),
+            'shipping_id' => null // Set shipping_id to null since we're using address_id instead
+        ]);
+        foreach( $request->items as $product){
+            OrderItem::create([
+                'order_id' => $order->id,
+                'product_id'=> $product['product_id'],
+                'quantity'=> $product['quantity'],
+                'price'=> $product['price'],
+            ]);
+            $cart = Cart::find($product['cart_id']);
             if ($cart) {
-                $cart->order_id = $Order->id;
+                $cart->order_id = $order->id;
                 $cart->status='progress';
                 $cart->save();
             }
-            $orderIds[] = $Order->order_number;
-            $orderIdstirng = $Order->order_number; // Concatenate order numbers
         }
+        
+        $api = new Api(env('RAZOR_KEY_ID'), env('RAZOR_KEY_SECRET'));
+        $orderData = [
+            'receipt'         => $order->order_number,
+            'amount'          => ($order->total_amount*100), // Amount in paise
+            'currency'        => 'INR',
+            'payment_capture' => 1 // Auto capture
+        ];
+        $rzorder = $api->order->create($orderData);
 
-
-           $api = new Api(env('RAZOR_KEY_ID'), env('RAZOR_KEY_SECRET'));
-           $orderData = [
-               'receipt'         => $orderIdstirng,
-               'amount'          => $TOTALAMT, // Amount in paise
-               'currency'        => 'INR',
-               'payment_capture' => 1 // Auto capture
-           ];
-
-           $order = $api->order->create($orderData);
-           return response()->json([
-            'orderIds' => $order // Convert the order object to an array
-            ]);
+        return response()->json([
+            'rzorderIds' => $rzorder,
+            'orderId' => $order->order_number  
+        ]);
     }
 
     public function paychecksave(Request $request){
